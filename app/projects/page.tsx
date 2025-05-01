3bbf7b162e5b3b9cff9015523fb2e385dea80e35
nadeem <nadeemsani786@hotmail.com>
2025-05-01 06:56:46 +0530

    latest59

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,17 +7,15 @@ import { BillionStars } from "../models/BillionStars";
 import HomeModel from "../components/HomeIcon3D";
 import ProjectStars from "../components/ProjectStars";
 import ProjectModal from "../components/ProjectModal";
+import Project from "../components/ProjectModal"
 
 export default function Page4() {
-  const [selectedProject, setSelectedProject] = useState(null);
+  const [selectedProject, setSelectedProject] = useState<Project | null>(null);
+
 
   return (
     <div className="w-screen h-screen fixed top-0 left-0">
-      <Canvas camera={{ position: [0, 10, 30], fov: 50 }}
-
-
-      >
-
+      <Canvas camera={{ position: [0, 10, 30], fov: 50 }}>
         <ambientLight intensity={1} />
         <directionalLight position={[5, 5, 5]} intensity={2} />
         <Suspense fallback={<Html>Loading...</Html>}>
@@ -33,3 +31,9 @@ export default function Page4() {
     </div>
   );
 }
+type Project = {
+  name: string;
+  description: string;
+  tech: string[];
+  link: string;
+};
